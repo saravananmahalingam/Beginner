e6bb60123f8ed02e5b3f3d6fe3f16f227d05780b
Saravanan.M <46522992+saravananmahalingam@users.noreply.github.com>
2019-01-21 23:25:47 +0530

    Update N_times_first5multiples.py

--- N_times_first5multiples.py
+++ N_times_first5multiples.py
@@ -1,5 +1,11 @@
 n=int(input())
 m=0
+li=[]
 for i in range(1,6):
 	m=n*i
-	print(m,end=" ")
+	li.append(m)
+for i in range(len(li)):
+	if i==len(li)-1:
+		print(li[i],end="")
+	else:
+		print(li[i],end=" ")
