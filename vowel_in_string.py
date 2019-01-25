a7dcba82f5d5adb6b752aaba9a095db900afb15b
Saravanan.M <46522992+saravananmahalingam@users.noreply.github.com>
2019-01-25 16:26:30 +0530

    Update vowel_in_string.py

--- vowel_in_string.py
+++ vowel_in_string.py
@@ -1,13 +1,10 @@
 a=input()
 c=0
 l=['a','e','i','o','u','A','E','I','O','U']
-if a.isalpha():
-	for i in a:
-		if i in l:
-			c=c+1
-	if c!=0:
-		print("yes")
-	else:
-		print("no")
+for i in a:
+	if i in l:
+		c=c+1
+if c!=0:
+	print("yes")
 else:
 	print("no")
