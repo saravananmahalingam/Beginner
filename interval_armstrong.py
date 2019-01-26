f335811a90322d31bf50ea0fc11e9b5768c30385
Saravanan.M <46522992+saravananmahalingam@users.noreply.github.com>
2019-01-26 12:51:27 +0530

    Update interval_armstrong.py

--- interval_armstrong.py
+++ interval_armstrong.py
@@ -1,10 +1,13 @@
 a,b=map(int,input().split())
+x=[]
 for i in range(a,b):
 	c=i
 	s1=0
+	e=len(str(i))
 	while c>0:
 		d=c%10
-		s1=s1+d**3
+		s1=s1+d**e
 		c=c//10
-		if s1==i:
-			print(i)
+	if s1==i:
+		x.append(i)
+print(" ".join(map(str,x)))
